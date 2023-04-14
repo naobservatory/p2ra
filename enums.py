438091fda8fd8f95215e9f673871e46942f3fbcb
simonleandergrimm <simonleandergrimm@gmail.com>
2023-04-14 15:38:11 -0400

    Uploading enums.py
    enums.py defines two enums, one called NAType, the other EnvelopedNonEnveloped.
    They force one to specify NAType as DNA or RNA, and EnvelopedNonEnveloped
    as enveloped or non_enveloped.

---
+++ enums.py
@@ -0,0 +1,15 @@
+from enum import Enum
+#Enums, short for enumerations, are a data type in Python used to represent a set of named values, which are typically used to define a set of related constants with unique names.
+
+#Defining an enumerator for a pathogen's nucleic acid type
+class NAType(Enum):
+	dna = 'DNA'
+	rna = 'RNA'
+	DNA = 'DNA'
+	RNA = 'RNA'
+
+#Defining an enumerator for a pathogen's enveloped or non-enveloped status
+class EnvelopedNonEnveloped(Enum):
+	enveloped = 'enveloped'
+	non_enveloped = 'non_enveloped'
+
