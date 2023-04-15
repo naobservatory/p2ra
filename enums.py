40670e490ed40f4539e05c9e7d224441fac1f633
simonleandergrimm <simonleandergrimm@gmail.com>
2023-04-15 18:03:44 -0400

    Reformatted enums.py along Black style guide

--- enums.py
+++ enums.py
@@ -1,14 +1,15 @@
 from enum import Enum
+
 # Enums, short for enumerations, are a data type in Python used to represent a set of named values,
 # which are typically used to define a set of related constants with unique names.
 
-#Defining an enumerator for a pathogen's nucleic acid type
+# Defining an enumerator for a pathogen's nucleic acid type
 class NAType(Enum):
-	DNA = 'DNA'
-	RNA = 'RNA'
+    DNA = "DNA"
+    RNA = "RNA"
 
-#Defining an enumerator for a pathogen's enveloped or non-enveloped status
-class EnvelopedNonEnveloped(Enum):
-	enveloped = 'enveloped'
-	non_enveloped = 'non_enveloped'
 
+# Defining an enumerator for a pathogen's enveloped or non-enveloped status
+class enveloped(Enum):
+    enveloped = "enveloped"
+    non_enveloped = "non_enveloped"
