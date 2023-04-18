97c6344a50d6f3671b58691cda208d60a56ee632
simonleandergrimm <simonleandergrimm@gmail.com>
2023-04-18 10:05:21 -0400

    Edited hsv-1 prevalence to be leaner and changed enums.py to  pathogen_properties.py, which now also creates dataclasses for pathogen ccharacters and prevalence variables

--- hsv_1_prevalence.py
+++ hsv_1_prevalence.py
@@ -1,11 +1,7 @@
-# HSV_1 prevalence calculation
-# Written by Simon
+#!/usr/bin/env python3
 
-# Importing enums and math
-from enums import *
+from pathogen_properties import *
 import math
-
-# ============================
 
 background = """Herpes Simplex Virus 1 is a very common herpesvirus that causes oral herpes (CDC, https://www.
 hopkinsmedicine.org/health/conditions-and-diseases/herpes-hsv1-and-hsv2), manifesting as cold sores or fever
@@ -13,44 +9,35 @@ blisters (https://www.hopkinsmedicine.org/health/conditions-and-diseases/herpes-
 infection and potential symptoms, most HSV-1 infections persist lifelong."""
 
 
-pathogen_chars = {
-    "pathogen_name": "Herpes Simplex Virus 1",
-    "pathogen_acronym": "HSV-1",
-    "na_type": NAType.DNA.value,
-    "enveloped_non_enveloped": enveloped.non_enveloped.value,
-    "ncbi_taxid": 10298,
-}
+pathogen_chars = PathogenChars(
+    na_type=NAType.DNA.value,
+    enveloped=Enveloped.NON_ENVELOPED.value,
+    taxid=10298,
+)
 
 
 prevalence_vars = {
-    # In a the 2015-2016 NHANES survey, the seroprevalence of HSV-1 was 49.6% in sample of 3710 people:
-    "prevalence_var_1": {
-        "variable_name": "cdc_2015_2016_nhanes_seroprevalence",
-        "variable_type": "measurement",
-        "percentage": 0.496,
-        "number_of_participants": 3710,
-        "source": "https://web.archive.org/web/20220707050306/https://wwwn.cdc.gov/Nchs/Nhanes/2015-2016/HSV_I.htm",
-    },
-    # Based on the seroprevalence data above, the CDC estimated the prevalence of HSV-1:
-    "prevalence_var_2": {
-        "variable_name": "cdc_2015_2016_nhanes_estimate",
-        "variable_type": "external_estimate",
-        "percentage": 0.478,
-        "confidence_interval": (0.4281, 0.5277),
-        "source": "https://www.cdc.gov/nchs/data/databriefs/db304_table.pdf#page=1",
-        "methods": "https://www.cdc.gov/nchs/products/databriefs/db304.htm#:~:text=Data%20for%20this,p%20%3C%200.05",
-    },
-    # In a 2015 study, 50 individuals self-collected tear and saliva samples twice daily which was tested for HSV 1
-    # DNA. 23 Individuals reported prior herpetic disease The sample alse skewed toward African Americans (n=37; 78%).
-    # Across the 50-day study period, 49 out of 50 subjects shed HSV DNA atleast once.
-    "prevalence_var_3": {
-        "variable_name": "tear_and_saliva_prevalence",
-        "variable_type": "measurement",
-        "percentage": 0.98,  # percentage prevalence
-        "number_of_participants": 50,  # Number of people tested
-        "source": "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC1200985/#:~:text=Frequency%20of%20HSV%2D1%20DNA%20shedding%20over,swab%20data%20(subjects%2030%2C%2038%20not%20available).",
-    },
+    "cdc_2015_2016_nhanes_seroprevalence": PrevalenceVariable(
+        variable_type=VariableType.MEASUREMENT.value,
+        percentage=0.496,
+        number_of_participants=3710,
+        country="United States",
+        source="https://web.archive.org/web/20220707050306/https://wwwn.cdc.gov/Nchs/Nhanes/2015-2016/HSV_I.htm",
+    ),
+    "cdc_2015_2016_nhanes_estimate": PrevalenceVariable(
+        variable_type=VariableType.EXTERNAL_ESTIMATE.value,
+        percentage=0.478,
+        country="United States",
+        confidence_interval=(0.4281, 0.5277),
+        source="https://www.cdc.gov/nchs/data/databriefs/db304_table.pdf#page=1",
+        methods="https://www.cdc.gov/nchs/products/databriefs/db304.htm#:~:text=Data%20for%20this,p%20%3C%200.05",
+    ),
+    "tear_and_saliva_prevalence": PrevalenceVariable(
+        variable_type=VariableType.MEASUREMENT.value,
+        percentage=0.98,
+        number_of_participants=50,
+        country="United States",
+        state="Louisiana",
+        source="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC1200985/#:~:text=Frequency%20of%20HSV%2D1%20DNA%20shedding%20over,swab%20data%20(subjects%2030%2C%2038%20not%20available).",
+    ),
 }
-
-# Making variables and functions available to other scripts
-__all__ = ["pathogen_chars", "prevalence_vars", "background"]
---
+++ pathogen_properties.py
@@ -0,0 +1,43 @@
+from enum import Enum
+from dataclasses import dataclass, asdict
+from typing import Optional, Tuple
+
+# Enums, short for enumerations, are a data type in Python used to represent a set of named values,
+# which are typically used to define a set of related constants with unique names.
+
+# TODO: Potentially create enum for country, state, etc. to ensure consistency in naming
+
+
+class NAType(Enum):
+    DNA = "DNA"
+    RNA = "RNA"
+
+
+class Enveloped(Enum):
+    ENVELOPED = "enveloped"
+    NON_ENVELOPED = "non_enveloped"
+
+
+class VariableType(Enum):
+    MEASUREMENT = "measurement"
+    EXTERNAL_ESTIMATE = "external_estimate"
+    NAO_ESTIMATE = "nao_estimate"
+
+
+@dataclass
+class PathogenChars:
+    na_type: NAType
+    enveloped: Enveloped
+    taxid: int
+
+
+@dataclass
+class PrevalenceVariable:
+    variable_type: VariableType
+    percentage: float
+    source: str
+    country: str
+    state: str = None
+    number_of_participants: int = None
+    confidence_interval: Tuple[float, float] = None
+    methods: str = None
