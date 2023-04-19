9f9e741767fcbf1780451f6e0e135cfdfc1017f3
Jeff Kaufman <jeff@jefftk.com>
2023-04-19 14:58:53 -0400

    test: remove redundant path cleanup

--- pathogens/__init__.py
+++ pathogens/__init__.py
@@ -2,8 +2,8 @@ import os
 import importlib
 
 pathogens = {}
-for pathogen_fname in os.listdir(os.path.abspath(os.path.dirname(__file__))):
-    pathogen_name, ext = os.path.splitext(os.path.basename(pathogen_fname))
+for pathogen_fname in os.listdir(os.path.dirname(__file__)):
+    pathogen_name, ext = os.path.splitext(pathogen_fname)
     if pathogen_name == "__init__": continue
     if ext != ".py": continue
 
