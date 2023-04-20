f082c29a1740e00dfe4000e2496deef32bc98a4c
simonleandergrimm <simonleandergrimm@gmail.com>
2023-04-20 15:21:39 -0400

    Adapted suggestions suggested by Jeff (https://github.com/naobservatory/p2ra/pull/4#pullrequestreview-1393098372)

--- pathogens/hva.py
+++ pathogens/hva.py
@@ -1,16 +1,12 @@
-import sys
-
-sys.path.append("..")
-
 from p2ra_prevalences.pathogen_properties import *
-import math
+
 
 background = """Hepatitis A is a vaccine-preventable liver-infection caused by the hepatitis A virus. In the US,
 it's mostly spread by individual contact. There is little seasonal variance in Hepatitis A incidence (https://www.cdc.gov/vaccines/pubs/pinkbook/hepa.html). Viral shedding persists for 1 to 3 weeks."""
 
 pathogen_chars = PathogenChars(
-    na_type=NAType.RNA.value,
-    enveloped=Enveloped.NON_ENVELOPED.value,
+    na_type=NAType.RNA,
+    enveloped=Enveloped.NON_ENVELOPED,
     taxid=208726,
 )
 
@@ -29,18 +25,19 @@ prevalence_est = {  # prevalence estimators
         unit="people",
         value_type="population",
         country="United States",
-        source="https://www.google.com/search?q=us+population+2018&oq=us+population+2018&aqs=chrome.0.69i59j0i512j0i22i30l6j0i390i650l2.5937j1j7&sourceid=chrome&ie=UTF-8",
+        source="https://www.google.com/search?q=us+population+2018",
         start_date="2018-01-01",
         end_date="2018-12-31",
     ),
     "hva_shedding_duration": PrevalenceEstimator(
         value=14,
+        confidence_interval=(7, 21),
         unit="days",
         value_type="shedding_duration",
         country="United States",
         source="https://www.cdc.gov/vaccines/pubs/pinkbook/hepa.html#:~:text=Viral%20shedding%20persists%20for%201%20to%203%20weeks.",
     ),
-    "king_county_incidence_rate_2017": PrevalenceEstimator(
+    "king_county_confirmed_cases_rate_2017": PrevalenceEstimator(
         value=0.5,
         unit="cases_per_100k",
         value_type="incidence_rate",
@@ -51,7 +48,7 @@ prevalence_est = {  # prevalence estimators
         end_date="2017-12-31",
         source="https://doh.wa.gov/sites/default/files/2023-01/420-004-CDAnnualReport2021.pdf?uid=642c448518316#page=28",
     ),
-    "king_county_incidence_rate_2018": PrevalenceEstimator(
+    "king_county_confirmed_cases_rate_2018": PrevalenceEstimator(
         value=0.6,
         unit="cases_per_100k",
         value_type="incidence_rate",
