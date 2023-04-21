a158a8e731fdae3fea0a46ec9cb8484ce9653636
Jeff Kaufman <jeff@jefftk.com>
2023-04-20 21:06:46 -0400

    update summarize for estimates no longer having names

--- summarize.py
+++ summarize.py
@@ -6,11 +6,8 @@ import pathogens
 def start():
     for pathogen_name, pathogen in pathogens.pathogens.items():
         print(pathogen_name)
-        for estimate_name, estimate in pathogen.estimate_prevalences().items():
-            print(
-                "  %s: %.2f per 100k"
-                % (estimate_name, estimate.infections_per_100k)
-            )
+        for estimate in pathogen.estimate_prevalences():
+            print("  %.2f per 100k" % estimate.infections_per_100k)
 
 
 if __name__ == "__main__":
