27acd2a09e8fc85485f8c12175a617a2b986a460
simonleandergrimm <simonleandergrimm@gmail.com>
2023-04-21 13:14:59 -0400

    Fixed code during review with Jeff

--- pathogens/hva.py
+++ pathogens/hva.py
@@ -10,7 +10,7 @@ pathogen_chars = PathogenChars(
 )
 
 
-us_incidence_absolute_2018: IncidenceAbsolute(
+us_incidence_absolute_2018 = IncidenceAbsolute(
     annual_infections=12474,
     confidence_interval=(17500, 27400),
     country="United States",
@@ -19,7 +19,7 @@ us_incidence_absolute_2018: IncidenceAbsolute(
     end_date="2018-12-31",
 )
 
-us_population_2018: Population(
+us_population_2018 = Population(
     people=327.2 * 1e6,
     country="United States",
     source="https://data.census.gov/table?q=2018+us+population&t=Civilian+Population",
@@ -27,21 +27,21 @@ us_population_2018: Population(
     end_date="2018-12-31",
 )
 
-hva_shedding_duration: SheddingDuration(
+hva_shedding_duration = SheddingDuration(
     days=14,
     confidence_interval=(7, 21),
     country="United States",
     source="https://www.cdc.gov/vaccines/pubs/pinkbook/hepa.html#:~:text=Viral%20shedding%20persists%20for%201%20to%203%20weeks.",
 )
 
-incidence_underreporting_scalar: Scalar(
+incidence_underreporting_scalar = Scalar(
     scalar=1 / 0.59,
     confidence_interval=(1 / 0.32, 1 / 0.84),
     country="United States",
     source="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4906888/#:~:text=Diagnosed%20hepatitis%20A,clear%20reporting%20responsibilities.",
 )
 
-king_county_absolute_2017: IncidenceAbsolute(
+king_county_absolute_2017 = IncidenceAbsolute(
     annual_infections=11,
     country="United States",
     state="Washington",
@@ -51,7 +51,7 @@ king_county_absolute_2017: IncidenceAbsolute(
     source="https://doh.wa.gov/sites/default/files/2023-01/420-004-CDAnnualReport2021.pdf?uid=642c448518316#page=28",
 )
 
-king_county_absolute_2018: IncidenceAbsolute(
+king_county_absolute_2018 = IncidenceAbsolute(
     annual_infections=14,
     country="United States",
     state="Washington",
@@ -61,7 +61,7 @@ king_county_absolute_2018: IncidenceAbsolute(
     source="https://doh.wa.gov/sites/default/files/2023-01/420-004-CDAnnualReport2021.pdf?uid=642c448518316#page=28",
 )
 
-king_county_confirmed_cases_rate_2017: IncidenceRate(
+king_county_confirmed_cases_rate_2017 = IncidenceRate(
     annual_infections_per_100k=0.5,
     country="United States",
     state="Washington",
@@ -72,7 +72,7 @@ king_county_confirmed_cases_rate_2017: IncidenceRate(
 )
 
 
-king_county_confirmed_cases_rate_2018: IncidenceRate(
+king_county_confirmed_cases_rate_2018 = IncidenceRate(
     annual_infections_per_100k=0.6,
     country="United States",
     state="Washington",
@@ -84,18 +84,14 @@ king_county_confirmed_cases_rate_2018: IncidenceRate(
 
 
 def estimate_prevalences():
-    return {
-        us_prevalence_2018: us_incidence_absolute_2018.to_rate(
-            us_population_2018
-        ).to_prevalence(hva_shedding_duration),
-        king_county_prevalence_2017: king_county_confirmed_cases_rate_2015.to_prevalence(
+    return [
+        us_incidence_absolute_2018.to_rate(us_population_2018).to_prevalence(
             hva_shedding_duration
-        ).scale(
-            incidence_underreporting_scalar
         ),
-        king_county_prevalence_2018: king_county_confirmed_cases_rate_2018.to_prevalence(
+        king_county_confirmed_cases_rate_2017.to_prevalence(
             hva_shedding_duration
-        ).scale(
-            incidence_underreporting_scalar
-        ),
-    }
+        ).scale(incidence_underreporting_scalar),
+        king_county_confirmed_cases_rate_2018.to_prevalence(
+            hva_shedding_duration
+        ).scale(incidence_underreporting_scalar),
+    ]
