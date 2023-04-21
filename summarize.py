3e2aaff940ad16b14de46bd2ab03c2fb4de9a386
Jeff Kaufman <jeff@jefftk.com>
2023-04-21 10:48:55 -0400

    Add summarize_location and summarize_date
    Variables have date and location information that has varying levels of precision, and can represent multiple sources if they're composite variables.  Add methods to summarize these.

--- summarize.py
+++ summarize.py
@@ -8,9 +8,17 @@ def start(pathogen_names):
     for pathogen_name, pathogen in pathogens.pathogens.items():
         if pathogen_names and pathogen_name not in pathogen_names:
             continue
+
         print(pathogen_name)
         for estimate in pathogen.estimate_prevalences():
-            print("  %.2f per 100k" % estimate.infections_per_100k)
+            print(
+                "  %.2f per 100k (%s; %s)"
+                % (
+                    estimate.infections_per_100k,
+                    estimate.summarize_location(),
+                    estimate.summarize_date(),
+                )
+            )
 
 
 if __name__ == "__main__":
