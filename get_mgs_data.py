406df0f838a24371f00d02554eb3f59b28e3cdf9
Daniel P. Rice <daniel.paul.rice@gmail.com>
2023-04-25 14:38:58 -0400

    Organize loading into functions

--- get_mgs_data.py
+++ get_mgs_data.py
@@ -1,13 +1,38 @@
 import json
 from pathlib import Path
-from typing import NewType
+from typing import Any, NewType
 
-from pathogens import pathogens
 from pathogen_properties import TaxID
+from pathogens import pathogens
 
-for k, v in pathogens.items():
-    print(k)
-    print(v.pathogen_chars.taxid)
+Sample = NewType("Sample", str)
+
+
+def load_samples(mgs_dir: Path, bioproject: str) -> list[Sample]:
+    with open(mgs_dir / "metadata_bioprojects.json") as projects_file:
+        data = json.load(projects_file)
+    return [Sample(s) for s in data[bioproject]]
+
+
+SampleAttributes = dict[str, str]
+
+
+def load_sample_attributes(
+    mgs_dir: Path, samples: list[Sample]
+) -> dict[Sample, SampleAttributes]:
+    with open(mgs_dir / "metadata_samples.json") as samples_file:
+        data = json.load(samples_file)
+    return {s: data[s] for s in samples}
+
+
+def load_sample_counts(mgs_dir: Path) -> dict[TaxID, dict[Sample, int]]:
+    with open(mgs_dir / "human_virus_sample_counts.json") as data_file:
+        data: dict[str, dict[str, int]] = json.load(data_file)
+    return {
+        TaxID(int(taxid)): {Sample(sample): n for sample, n in counts.items()}
+        for taxid, counts in data.items()
+    }
+
 
 pathogen = "sars_cov_2"
 taxid = pathogens[pathogen].pathogen_chars.taxid
@@ -16,32 +41,10 @@ print(taxid)
 # Rothman
 bioproject = "PRJNA729801"
 data_dir = Path("../mgs-pipeline/dashboard/")
-
-Sample = NewType("Sample", str)
-
-with open(data_dir / "metadata_bioprojects.json") as projects_file:
-    meta_project = json.load(projects_file)
-samples = meta_project[bioproject]
-
-
-with open(data_dir / "metadata_samples.json") as samples_file:
-    meta_samples = json.load(samples_file)
-
-sample_attributes = {s: meta_samples[s] for s in samples}
-
-
-def load_sample_counts(mgs_dir: Path) -> dict[TaxID, dict[Sample, int]]:
-    with open(mgs_dir / "human_virus_sample_counts.json") as data_file:
-        data = json.load(data_file)
-    return {
-        TaxID(taxid): {Sample(sample): n for sample, n in counts.items()}
-        for taxid, counts in data.items()
-    }
-
-
+samples = load_samples(data_dir, bioproject)
+sample_attribs = load_sample_attributes(data_dir, samples)
+for k, v in sample_attribs.items():
+    print(k)
+    print(v)
 counts = load_sample_counts(data_dir)
-x: Sample
-for k in counts[TaxID(5)].keys():
-    x = k
-
-print(counts)
+print(counts[taxid])
