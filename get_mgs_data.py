15b8bdb1e7172f8bc033dd287efcd8eb93c05a1c
Daniel P. Rice <daniel.paul.rice@gmail.com>
2023-04-25 17:40:43 -0400

    Get subtree and count reads per sample

--- get_mgs_data.py
+++ get_mgs_data.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import json
+from collections import Counter
 from dataclasses import dataclass, field
 from datetime import date
 from pathlib import Path
@@ -15,7 +16,9 @@ Sample = NewType("Sample", str)
 
 
 def load_samples(mgs_dir: Path, bioproject: str) -> list[Sample]:
-    with open(mgs_dir / "metadata_bioprojects.json") as projects_file:
+    with open(
+        mgs_dir / "dashboard/metadata_bioprojects.json"
+    ) as projects_file:
         data = json.load(projects_file)
     return [Sample(s) for s in data[bioproject]]
 
@@ -31,13 +34,18 @@ class SampleAttributes(BaseModel):
 def load_sample_attributes(
     mgs_dir: Path, samples: list[Sample]
 ) -> dict[Sample, SampleAttributes]:
-    with open(mgs_dir / "metadata_samples.json") as samples_file:
+    with open(mgs_dir / "dashboard/metadata_samples.json") as samples_file:
         data = json.load(samples_file)
     return {s: SampleAttributes(**data[s]) for s in samples}
 
 
-def load_sample_counts(mgs_dir: Path) -> dict[TaxID, dict[Sample, int]]:
-    with open(mgs_dir / "human_virus_sample_counts.json") as data_file:
+SampleCounts = dict[TaxID, dict[Sample, int]]
+
+
+def load_sample_counts(mgs_dir: Path) -> SampleCounts:
+    with open(
+        mgs_dir / "dashboard/human_virus_sample_counts.json"
+    ) as data_file:
         data: dict[str, dict[str, int]] = json.load(data_file)
     return {
         TaxID(int(taxid)): {Sample(sample): n for sample, n in counts.items()}
@@ -50,6 +58,35 @@ class TaxTree:
     taxid: TaxID
     children: list[TaxTree] = field(default_factory=list)
 
+    def __getitem__(self, taxid: TaxID) -> TaxTree | None:
+        return get_subtree(self, taxid)
+
+
+def get_subtree(taxtree: TaxTree, taxid: TaxID) -> TaxTree | None:
+    """Depth-first search for taxid"""
+    if taxtree.taxid == taxid:
+        return taxtree
+    else:
+        for child in taxtree.children:
+            subtree = get_subtree(child, taxid)
+            if subtree:
+                return subtree
+        else:
+            return None
+
+
+def count_reads(
+    taxtree: TaxTree, sample_counts: SampleCounts
+) -> Counter[Sample]:
+    if taxtree.taxid in sample_counts:
+        c = Counter(sample_counts[taxtree.taxid])
+    else:
+        c = Counter()
+    return sum(
+        (count_reads(child, sample_counts) for child in taxtree.children),
+        start=c,
+    )
+
 
 def _parse_taxtree(input: list) -> TaxTree:
     taxid = TaxID(int(input[0]))
@@ -58,25 +95,34 @@ def _parse_taxtree(input: list) -> TaxTree:
 
 
 def load_tax_tree(mgs_dir: Path) -> TaxTree:
-    with open(mgs_dir / "human_virus_tree.json") as data_file:
+    with open(mgs_dir / "dashboard/human_virus_tree.json") as data_file:
         data = json.load(data_file)
     return _parse_taxtree(data)
 
 
-pathogen = "sars_cov_2"
-taxid = pathogens[pathogen].pathogen_chars.taxid
-print(taxid)
-
-# Rothman
-bioproject = "PRJNA729801"
-data_dir = Path("../mgs-pipeline/dashboard/")
+bioproject = "PRJNA729801"  # Rothman
+data_dir = Path("../mgs-pipeline/")
 samples = load_samples(data_dir, bioproject)
 sample_attribs = load_sample_attributes(data_dir, samples)
-for k, v in sample_attribs.items():
-    print(k)
-    print(v)
+fine_locs = set(sample_attribs[s].fine_location for s in samples)
 counts = load_sample_counts(data_dir)
-print(counts[taxid])
-
 taxtree = load_tax_tree(data_dir)
-print(taxtree)
+
+for pathogen in ["sars_cov_2", "norovirus"]:
+    print(pathogen)
+    taxid = pathogens[pathogen].pathogen_chars.taxid
+    subtree = taxtree[taxid]
+
+    if subtree:
+        virus_counts = count_reads(subtree, counts)
+        print("All", sum(virus_counts[s] for s in samples), sep="\t")
+        for fine_loc in fine_locs:
+            print(
+                fine_loc,
+                sum(
+                    virus_counts[s]
+                    for s in samples
+                    if sample_attribs[s].fine_location == fine_loc
+                ),
+                sep="\t",
+            )
