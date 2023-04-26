0df0574cac4df300001e09859bf8ad8c00c2ba89
Asher Parker-Sartori <aparkersartori@outlook.com>
2023-04-26 13:11:30 -0400

    Updated based on Simon's feedback -- Everything fixed afaik

--- pathogens/dengue.py
+++ pathogens/dengue.py
@@ -1,16 +1,19 @@
 from pathogen_properties import *
 
-background = """Dengue virus, a mosquito-borne viral infection primarily transmitted by the Aedes aegypti and Aedes albopictus mosquitoes, present in tropical and subtropical regions. The virus presents with flu-like symptoms that can progress to severe dengue, characterized by hemorrhagic fever, shock, and even death."""
+background = """Dengue virus, a mosquito-borne viral infection primarily transmitted
+by the Aedes aegypti and Aedes albopictus mosquitoes, present in tropical and subtropical regions.
+The virus presents with flu-like symptoms that can progress to severe dengue, characterized by
+hemorrhagic fever, shock, and even death."""
 
 
 pathogen_chars = PathogenChars(
     na_type=NAType.RNA,
     enveloped=Enveloped.ENVELOPED,
-    taxid=12814,
+    taxid=12637,
 )
 
-california_reported_cases = PrevalenceAbsolute(
-    infections=86,
+california_reported_cases = IncidenceAbsolute(
+    annual_infections=86,
     country="United States",
     state="California",
     date="2020",
@@ -19,28 +22,20 @@ california_reported_cases = PrevalenceAbsolute(
 )
 
 CA_population = Population(
-    people=39000000,
+    people=39538245,
     tag="California-2020",
+    source="https://www.census.gov/quickfacts/CA",
 )
 # disease duration in days
-disease_duration = Scalar(
-    scalar=7,
+disease_duration = SheddingDuration(
+    days=7,
     source="https://www.cdc.gov/dengue/symptoms/index.html",
-)
-
-per100k = Scalar(
-    scalar=100000,
-)
-
-reciprocal_days_in_a_year = Scalar(
-    scalar=1 / 365,
 )
 
 
 def estimate_prevalences():
     return [
-        california_reported_cases.to_rate(CA_population)
-        .scale(disease_duration)
-        .scale(per100k)
-        .scale(reciprocal_days_in_a_year)
+        california_reported_cases.to_rate(CA_population).to_prevalence(
+            disease_duration
+        )
     ]
