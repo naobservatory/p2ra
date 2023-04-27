4d15a45bdf90d5826f6867cf19b338ac613d0e4b
simonleandergrimm <simonleandergrimm@gmail.com>
2023-04-27 16:26:32 -0400

    Added pathogen_parser.py, a file which, as of now, should download NHANES files using wget

--- prevalence-data/nhanes/pathogen_parser.py
+++ prevalence-data/nhanes/pathogen_parser.py
@@ -1,21 +1,34 @@
 # File to download and parse NHANES data for Hepatitis A, B, and C Virus, HIV, HSV-1, HSV-2, and HPV.
 
 import subprocess
+import os
 
-url = "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/HEPA_J.XPT"
+# Create a new directory
 
-save_path = "hepa_j.xpt"
 
-subprocess.run(["wget", "-O", save_path, url], check=True)
+for year in ["2015-2016", "2017-2018"]:
+    if not os.path.exists(year):
+        os.mkdir(year)
 
-for year in ["2017-2018", "2015-2016"]:
-    url_w_year = f"https://wwwn.cdc.gov/Nchs/Nhanes/{year}/"
-    save_path = f"{year}"
-    for leaf in ["HEPA_J", "HEPBD", "HEPB_J", "HEPC_J", "HEPE_J", "HIV_J"]:
+    if year == "2015-2016" return "I" else return="J"
+
+
+    for leaf in [
+        f"HEPA_{end}",
+        f"HEPB_{end}",
+        f"HEPC_{end}",
+        f"HEPE_{end}",
+        f"HIV_{end}",
+        f"DEMO_{end}",
+        f"HSV_{end}",
+        f"HPVSWR_{end}",
+        f"HPVP_{end}",
+        f"OHRPV_{end}",
+    ]:
+        save_path = f"{year}/{leaf}"
+        url_w_year = f"https://wwwn.cdc.gov/Nchs/Nhanes/{year}/{leaf}.XPT"
         try:
-            subprocess.run(
-                ["wget", "-O", save_path, url_w_year + leaf], check=True
-            )
+            subprocess.run(["wget", "-O", save_path, url_w_year], check=True)
         except subprocess.CalledProcessError as e:
             print(
                 f"{leaf}.xpt is not available for the NHANES cycle {year}: {e}"
