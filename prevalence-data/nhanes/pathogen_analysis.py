1270e26bf7dca7db769580d2f3825370503f28a0
simonleandergrimm <simonleandergrimm@gmail.com>
2023-04-27 22:43:57 -0400

    Complete data acquisition and initial parsing of variables

---
+++ prevalence-data/nhanes/pathogen_analysis.py
@@ -0,0 +1,98 @@
+import csv
+import os
+from collections import Counter, defaultdict
+
+
+data_dictionary = {}
+sum = 0
+
+nhanes_cycles = ["2015-2016", "2017-2018"]
+
+for cycle in nhanes_cycles:
+    for f_name in os.listdir(f"{cycle}"):
+        # Set the first part of the file name as a new key in the dictionary
+        data_dictionary[f_name.split("_")[0]] = []
+
+print(data_dictionary)
+
+
+with open("2017-2018_variable_dict.tsv") as f:
+    reader = csv.reader(f, delimiter="\t")
+    keys = list(data_dictionary.keys())
+    print(keys)
+    for row in reader:
+        variable_abbrevation = row[2].split("_")[0]
+        if variable_abbrevation in keys:
+            print("yes")
+            # Adding all column values in that row to the dictionary
+            data_dictionary[variable_abbrevation].append(row[1:4])
+
+print(data_dictionary)
+
+# TODO: Add second variable_dict for 2015 and 2016 for the variable description
+
+# Pulling out rows in 2017-2018_variable_dict.tsv that match the variables
+
+# with open("2017-2018_variable_dict.tsv") as f:
+#     reader = csv.reader(f, delimiter="\t")
+#     for key in data_dictionary:
+#         for row in reader:
+#             if code == row[0]:
+#                 # Adding all column values in that row to the dictionary
+#                 data_dictionary[code].append(row[1])
+
+# data_dictionary = {code: [] for code in cols}
+
+# with open("2017-2018_variable_dict.tsv") as f:
+#     reader = csv.reader(f, delimiter="\t")
+#     for row in reader:
+#         code = row[0]
+#         if code in data_dictionary:
+#             # Adding all column values in that row to the dictionary
+#             data_dictionary[code].append(row[1])
+
+
+# print(data_dictionary)
+
+# # demographics = {}
+
+# # with open("data/sources/csv/2015-2016/DEMO_I.csv") as f:
+# #     reader = csv.reader(f)
+# #     cols = None
+# #     for row in reader:
+# #         if not cols:
+# #             cols = row
+# #         else:
+# #             demographics[row[cols.index("seqn")]] = {
+# #                 "age": row[cols.index("ridageyr")],
+# #             }
+# # rates = defaultdict(Counter)
+# # with open("data/sources/csv/2015-2016/ORHPV_I.csv") as f:
+# #     reader = csv.reader(f)
+
+# #     cols = None
+# #     for row in reader:
+# #         if not cols:
+# #             cols = row
+# #         else:
+# #             overall = "3"
+# #             for col, val in zip(cols, row):
+# #                 if col in ["seqn", "orxhpv", "orxgh", "orxgl"]:
+# #                     continue
+# #                 if val == "1":
+# #                     overall = val
+# #                 elif val == "2" and overall != "1":
+# #                     overall = val
+# #             if overall != row[cols.index("orxhpv")]:
+# #                 print(
+# #                     "ERROR: %s %s %r"
+# #                     % (overall, row[cols.index("orxhpv")], row)
+# #                 )
+
+# #             rates[demographics[row[cols.index("seqn")]]["age"]][overall] += 1
+
+# # for age in sorted(rates):
+# #     positives = rates[age]["1"]
+# #     negatives = rates[age]["2"]
+# #     total = positives + negatives
+# #     print("%s\t%s\t%s\t%s" % (age, positives, negatives, total))
---
+++ prevalence-data/nhanes/pathogen_download.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Download xpt2csv.py
+wget -O xpt2csv.py https://raw.githubusercontent.com/mirador/nhanes/master/xpt2csv.py
+
+# Loop over years
+for year in "2015-2016" "2017-2018"; do
+    # Create year directory if it doesn't exist
+    mkdir -p "$year"
+
+    # Set the end variable based on the year
+    if [ "$year" == "2017-2018" ]; then
+        end="J"
+    else
+        end="I"
+    fi
+
+    # Loop over leaf names and download files
+    for leaf in "HEPA_${end}" "HEPB_${end}" "HEPC_${end}" "HEPE_${end}" "HIV_${end}" "DEMO_${end}" "HSV_${end}" "HPVSWR_${end}" "HPVP_${end}" "OHRPV_${end}"; do
+        save_path="${year}/${leaf}.XPT"
+        url_w_year="https://wwwn.cdc.gov/Nchs/Nhanes/${year}/${leaf}.XPT"
+
+        # Download the file, skip if it already exists
+        wget -nc -O "$save_path" "$url_w_year"
+    done
+
+    # Delete all files that contain "Oops" in their content
+    grep -l "Oops" "${year}"/*.XPT | xargs rm
+
+    # Run the xpt2csv.py script
+    python xpt2csv.py "${year}" "${year}"
+
+    # Remove all XPT files
+    rm "${year}"/*.XPT
+done
