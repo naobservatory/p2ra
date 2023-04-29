63b5cabd5b874655b21ef853711d341fd9b15e4f
simonleandergrimm <simonleandergrimm@gmail.com>
2023-04-29 00:44:04 -0400

    Added a further pathogen to pathogen_analysis.py

--- prevalence-data/nhanes/pathogen_analysis.py
+++ prevalence-data/nhanes/pathogen_analysis.py
@@ -1,5 +1,7 @@
 import csv
 
+import os
+
 from collections import Counter, defaultdict
 
 demographics = {}
@@ -14,8 +16,10 @@ with open("2015-2016/DEMO_I.csv") as f:
             demographics[row[cols.index("seqn")]] = {
                 "age": row[cols.index("ridageyr")],
             }
+
 rates = defaultdict(Counter)
-with open("2015-2016/ORXHPV_I.csv") as f:
+
+with open("2015-2016/ORHPV_I.csv") as f:
     reader = csv.reader(f)
 
     cols = None
@@ -36,13 +40,57 @@ with open("2015-2016/ORXHPV_I.csv") as f:
                     "ERROR: %s %s %r"
                     % (overall, row[cols.index("orxhpv")], row)
                 )
-
             rates[demographics[row[cols.index("seqn")]]["age"]][overall] += 1
+# for age in sorted(rates):
+#     positives = rates[age]["1"]
+#     negatives = rates[age]["2"]
+#     total = positives + negatives
+#     print("%s\t%s\t%s\t%s" % (age, positives, negatives, total))
 
-for age in sorted(rates):
-    positives = rates[age]["1"]
-    negatives = rates[age]["2"]
+
+# rates = defaultdict(Counter)
+
+
+HEPA_15_16_rates = defaultdict(Counter)
+
+
+with open("2015-2016_variable_dict.tsv") as f:
+    reader = csv.reader(f, delimiter="\t")
+    cols = None
+    # list of all variables in the 2015-2016 data, without "_I.csv"
+    vars_2015 = [
+        fname.removesuffix(".csv") for fname in os.listdir("2015-2016")
+    ]
+    print(vars_2015)
+    with open("2015-2016_relevant_variables.tsv", "w") as f:
+        writer = csv.writer(f, delimiter="\t")
+        for row in reader:
+            if not cols:
+                cols = row
+                writer.writerow(row)
+            else:
+                if row[cols.index("Data File Name")] in vars_2015:
+                    writer.writerow(row)
+        # Turn a list of lists into a csv file
+    with open("2015-2016/HEPA_I.csv") as f:
+        reader = csv.reader(f)
+        cols = None
+        for row in reader:
+            if not cols:
+                cols = row
+                print(cols)
+            else:
+                hep_a_status = row[cols.index("lbxha")]
+
+                HEPA_15_16_rates[demographics[row[cols.index("seqn")]]["age"]][
+                    hep_a_status
+                ] += 1
+
+
+for age in sorted(HEPA_15_16_rates):
+    positives = HEPA_15_16_rates[age]["1"]
+    negatives = HEPA_15_16_rates[age]["2"]
+    indeterminate = HEPA_15_16_rates[age]["3"]
+    missing = HEPA_15_16_rates[age]["NA"]
     total = positives + negatives
     print("%s\t%s\t%s\t%s" % (age, positives, negatives, total))
-
-
