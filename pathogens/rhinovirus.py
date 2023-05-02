e34720c043d47ef0147626b40faf25f4059ea5e7
Asher Parker-Sartori <aparkersartori@outlook.com>
2023-05-02 13:54:09 -0400

    created rhinovirus branch

---
+++ pathogens/rhinovirus.py
@@ -0,0 +1,10 @@
+from pathogen_properties import *
+
+background = """  """
+
+
+pathogen_chars = PathogenChars(
+    na_type=NAType.RNA,
+    enveloped=Enveloped.NON_ENVELOPED,
+    taxid=TaxID(147711),
+)
