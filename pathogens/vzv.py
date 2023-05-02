0a1d31a82739408ab38cb6367ca792811fdf91b9
Asher Parker-Sartori <aparkersartori@outlook.com>
2023-05-02 14:36:37 -0400

    finished latent VZV & active chickenpox and shingles estimates

--- pathogens/vzv.py
+++ pathogens/vzv.py
@@ -65,23 +65,55 @@ prevalence_from_postvaccine_cases = Prevalence(
 )
 
 # prevaccine cases estimate + postvaccine cases estimate
-absolute_latent_prevalence = Prevalence(
+VZV_latent_prevalence = Prevalence(
     infections_per_100k=population_above_34.infections_per_100k
     + prevalence_from_postvaccine_cases.infections_per_100k,
     active=Active.LATENT,
 )
 
 
-annual_new_shingles_infections = Scalar(
-    scalar=1000000,
+# Now, calculations for active chickenpox and shingles estimates
+
+shingles_annual_infections = IncidenceAbsolute(
+    annual_infections=1000000,
+    tag="US-2023",
     source="https://www.cdc.gov/shingles/hcp/clinical-overview.html#:~:text=An%20estimated%20one%20million%20cases%20of%20herpes%20zoster%20occur%20annually%20in%20the%20United%20States",
 )
 
-active_prevalence = PrevalenceAbsolute(
-    infections=annual_new_shingles_infections * shingles_shedding_duration
-    + annual_new_VZV_infections * chickenpox_shedding_duration,
+chickenpox_annual_infections = IncidenceAbsolute(
+    annual_infections=annual_new_VZV_infections.scalar,
+    tag="US-2023",
+    source="https://www.childrenshospital.org/conditions/chickenpox#:~:text=More%20than%2095%20percent%20of%20American%20adults%20have%20had%20chickenpox%20and%20about%204%2C000%2C000%20people%20get%20chickenpox%20every%20year",
+)
+
+shingles_duration = SheddingDuration(
+    days=28,
+    source="https://www.nia.nih.gov/health/shingles#:~:text=Most%20cases%20of%20shingles%20last%20three%20to%20five%20weeks.",
+)
+
+chickenpox_duration = SheddingDuration(
+    days=12,
+    source="https://my.clevelandclinic.org/health/diseases/4017-chickenpox#:~:text=Chickenpox%20usually%20goes%20away%20after%2010%20to%2014%20days.",
+)
+
+shingles_active_prevalence = Prevalence(
+    infections_per_100k=shingles_annual_infections.to_rate(US_Population)
+    .to_prevalence(shingles_duration)
+    .infections_per_100k,
+    active=Active.ACTIVE,
+)
+
+chickenpox_active_prevalence = Prevalence(
+    infections_per_100k=chickenpox_annual_infections.to_rate(US_Population)
+    .to_prevalence(chickenpox_duration)
+    .infections_per_100k,
+    active=Active.ACTIVE,
 )
 
 
 def estimate_prevalences():
-    return [absolute_latent_prevalence]
+    return [
+        VZV_latent_prevalence,
+        chickenpox_active_prevalence,
+        shingles_active_prevalence,
+    ]
