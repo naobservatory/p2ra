39bbba403232303dc78a6fac32349adb69659dfe
simonleandergrimm <simonleandergrimm@gmail.com>
2023-05-02 20:40:38 -0400

    Initial commit of hcv.py

---
+++ pathogens/hcv.py
@@ -0,0 +1,5 @@
+from pathogen_properties import *
+
+background = """Hepatitis C is a chronic liver condition, caused by the hepatitis C virus. It is most often transmitted through IV drug use, but also through birth and sexual intercourse."""
+
+# TODO: Add case surveillance data: https://wonder.cdc.gov/nndss/static/2019/annual/2019-table2h.html
