30013d0696bad1f1f4b6059e7d3d8c595f17bba1
Asher Parker-Sartori <aparkersartori@outlook.com>
2023-05-03 15:37:04 -0400

    added more studies, changed methodology a little

--- pathogens/CMV.py
+++ pathogens/CMV.py
@@ -37,7 +37,7 @@ GER_adult_seroprevalence_estimate = Prevalence(
 )
 
 # This study is generally good, but also from around 20 years ago
-older_US_prevalence = Prevalence(
+overall_US_prevalence = Prevalence(
     infections_per_100k=0.504 * 100_000,
     country="US",
     active=Active.LATENT,
@@ -74,15 +74,49 @@ child_shedding = SheddingDuration(
     source="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4494736/#:~:text=children%20and%20adults.-,Table%203,-Duration%20of%20CMV",
 )
 
+US_child_proportion = Scalar(
+    scalar=0.222,
+    date="2022",
+    source="https://www.census.gov/quickfacts/fact/table/US#",
+)
 
-"""The CDC says that in the United States, nearly one in three children is
-already infected with CMV by age five.
-Over half of adults have been infected with CMV by age 40. I estimate that 
-children are around 38% prevalent on average, our previous 2 adult estimates
-imply that adults are around 56% prevalent on average. """
+US_adult_proportion = Scalar(
+    scalar=1 - US_child_proportion.scalar,
+    date="2022",
+    source="https://www.census.gov/quickfacts/fact/table/US#",
+)
+
+# Seroprevalence of children ages 1-5
+US_infant_seroprevalence = Prevalence(
+    infections_per_100k=0.207 * 100_000,
+    date="2011",
+    active=Active.LATENT,
+    source="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8248283/#:~:text=the%20seroprevalence%20of%20cytomegalovirus%20immunoglobulin%20G%20(IgG)%20antibodies%20among%20US%20children%20aged%201%E2%80%935%20years%20was%2020.7%25",
+)
+
+
+US_child_prevalence = Prevalence(
+    """To solve for the US_seroprevalence_of_children, we can simplify the 
+equation US_child_proportion * US_child_prevalence + US_adult_proportion * 
+new_US_adult_prevalence = overall_US_prevalence. 
+This gives us that US_child_prevalence = (older_US_prevalence - 
+US_adult_proportion * new_US_adult_prevalence) / US_child_proportion""",
+    # This number evaluates to 0.29. The seroprevalence of children ages 1-5 was
+    # 0.21, and it makes sense that this number is slightly lower than the overall
+    # child prevalence, since most children are older than 5
+    infections_per_100k=(
+        overall_US_prevalence - US_adult_proportion * new_US_adult_prevalence
+    )
+    / US_child_proportion,
+    date="2020",
+    country="US",
+    active=Active.LATENT,
+    source="",
+)
 
 total_days_shedding_per_person = Scalar(
-    scalar=0.2 * 0.5 * child_shedding.days + 0.8 * 0.5 * adult_shedding.days,
+    scalar=US_child_proportion * US_child_prevalence * child_shedding.days
+    + US_adult_proportion * new_US_adult_prevalence * adult_shedding.days,
     source="https://www.cdc.gov/cmv/overview.html#:~:text=nearly%20one%20in%20three%20children%20is%20already%20infected%20with%20CMV%20by%20age%20five.%20Over%20half%20of%20adults%20have%20been%20infected%20with%20CMV%20by%20age%2040.",
 )
 
@@ -101,4 +135,4 @@ shedding_prevalence = Prevalence(
 
 
 def estimate_prevalences():
-    return [GER_adult_seroprevalence_estimate, shedding_prevalence]
+    return [overall_US_prevalence, shedding_prevalence]
