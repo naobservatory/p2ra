naobservatory/p2ra

commit 1951f0f56f0df9c18e017ebb2784a011d437fe2f

infra: convert inputs to flat set (#73)
Prompted by Dan pointing out that add is non-associative in #67, switch inputs to be a flat set.  This means var.all_inputs will contain all inputs exactly once and you don't need recursion.

This required making our estimates hashable, which required setting frozen=True, which ran into a __post_init__ wart, which is why I'm using __setattr__ there

pathogen_properties.py
@@ -2,6 +2,7 @@ import calendar
 import datetime
 import os.path
 import re
+from collections.abc import Iterable
 from dataclasses import InitVar, dataclass, field
 from enum import Enum
 from typing import NewType, Optional
@@ -36,14 +37,14 @@ class Active(Enum):
 TaxID = NewType("TaxID", int)
 
 
-@dataclass(kw_only=True)
+@dataclass(kw_only=True, eq=True, frozen=True)
 class PathogenChars:
     na_type: NAType
     enveloped: Enveloped
     taxid: TaxID
 
 
-@dataclass(kw_only=True)
+@dataclass(kw_only=True, eq=True, frozen=True)
 class Variable:
     """An external piece of data"""
 
@@ -63,16 +64,21 @@ class Variable:
     parsed_start: Optional[datetime.date] = field(init=False)
     parsed_end: Optional[datetime.date] = field(init=False)
     is_target: Optional[bool] = False
-
-    # Remember to recursively consider each input's inputs if defined.
-    inputs: Optional[list["Variable"]] = None
+    inputs: InitVar[Optional[Iterable["Variable"]]] = None
+    all_inputs: set["Variable"] = field(init=False)
 
     def __post_init__(
         self,
         date: Optional[str],
         start_date: Optional[str],
         end_date: Optional[str],
+        inputs: Optional[Iterable["Variable"]],
     ):
+        # A python wart is that frozen dataclasses don't have an exception for
+        # __post_init__, and it thinks assignments here are mutation instead of
+        # initialization.  That's why we're assigning with __setattr__. See
+        # https://stackoverflow.com/questions/53756788/how-to-set-the-value-of-dataclass-field-in-post-init-when-frozen-true/54119384#54119384
+
         if date and (start_date or end_date):
             raise Exception("If you have start/end don't set date.")
         if (start_date and not end_date) or (end_date and not start_date):
@@ -80,15 +86,15 @@ class Variable:
         if date:
             start_date = end_date = date
 
+        parsed_start = None
         if start_date:
-            self.parsed_start = self._parse_date(start_date, "start")
-        else:
-            self.parsed_start = None
+            parsed_start = self._parse_date(start_date, "start")
+        object.__setattr__(self, "parsed_start", parsed_start)
 
+        parsed_end = None
         if end_date:
-            self.parsed_end = self._parse_date(end_date, "end")
-        else:
-            self.parsed_end = None
+            parsed_end = self._parse_date(end_date, "end")
+        object.__setattr__(self, "parsed_end", parsed_end)
 
         if (
             self.parsed_start
@@ -96,6 +102,11 @@ class Variable:
             and self.parsed_start > self.parsed_end
         ):
             raise Exception("Start date can't be after end date")
+
+        all_inputs = set(inputs or [])
+        for variable in set(inputs or []):
+            all_inputs |= variable.all_inputs
+        object.__setattr__(self, "all_inputs", frozenset(all_inputs))
 
     def _parse_date(self, date: str, start_or_end: str) -> datetime.date:
         y, m, d = None, None, None
@@ -135,38 +146,23 @@ class Variable:
             bits.append(self.country)
         return ", ".join(bits)
 
-    def _collect_locations(self, all_locations):
-        location = self._location()
-        if location:
-            all_locations.add(location)
-        if self.inputs and not self.is_target:
-            for variable in self.inputs:
-                variable._collect_locations(all_locations)
-
     def summarize_location(self, all_locations=None):
-        all_locations = set()
-        self._collect_locations(all_locations)
-        return "; ".join(sorted(all_locations))
-
-    def _collect_dates(self, all_dates: set[datetime.date]):
-        for date in [self.parsed_start, self.parsed_end]:
-            if date:
-                all_dates.add(date)
-        if self.inputs and not self.is_target:
-            for variable in self.inputs:
-                variable._collect_dates(all_dates)
+        return "; ".join(
+            sorted(
+                set(i._location() for i in self.all_inputs if i._location())
+            )
+        )
 
     def summarize_date(self) -> Optional[tuple[datetime.date, datetime.date]]:
-        all_dates: set[datetime.date] = set()
-        self._collect_dates(all_dates)
-
-        if not all_dates:
+        try:
+            return min(
+                i.parsed_start for i in self.all_inputs if i.parsed_start
+            ), max(i.parsed_end for i in self.all_inputs if i.parsed_end)
+        except ValueError:
             return None
 
-        return min(all_dates), max(all_dates)
 
-
-@dataclass(kw_only=True)
+@dataclass(kw_only=True, eq=True, frozen=True)
 class Population(Variable):
     """A number of people"""
 
@@ -176,12 +172,12 @@ class Population(Variable):
     tag: str
 
 
-@dataclass(kw_only=True)
+@dataclass(kw_only=True, eq=True, frozen=True)
 class Scalar(Variable):
     scalar: float
 
 
-@dataclass(kw_only=True)
+@dataclass(kw_only=True, eq=True, frozen=True)
 class Prevalence(Variable):
     """What fraction of people have this pathogen at some moment"""
 
@@ -205,7 +201,7 @@ class Prevalence(Variable):
         )
 
 
-@dataclass(kw_only=True)
+@dataclass(kw_only=True, eq=True, frozen=True)
 class PrevalenceAbsolute(Variable):
     """How many people had this pathogen at some moment"""
 
@@ -225,12 +221,12 @@ class PrevalenceAbsolute(Variable):
         )
 
 
-@dataclass(kw_only=True)
+@dataclass(kw_only=True, eq=True, frozen=True)
 class SheddingDuration(Variable):
     days: float
 
 
-@dataclass(kw_only=True)
+@dataclass(kw_only=True, eq=True, frozen=True)
 class Number(Variable):
     """Generic number.  Use this for weird one-off things
 
@@ -243,7 +239,7 @@ class Number(Variable):
         return Scalar(scalar=self.number / other.number, inputs=[self, other])
 
 
-@dataclass(kw_only=True)
+@dataclass(kw_only=True, eq=True, frozen=True)
 class IncidenceRate(Variable):
     """What fraction of people get this pathogen annually"""
 
@@ -262,7 +258,7 @@ class IncidenceRate(Variable):
         )
 
 
-@dataclass(kw_only=True)
+@dataclass(kw_only=True, eq=True, frozen=True)
 class IncidenceAbsolute(Variable):
     """How many people get this pathogen annually"""
 

commit daa2acd18da3aab701abac6fae63c85b88944501

norovirus: make estimates specifically for GI and GII (#60)
We want to be able to track the Norovirus subtypes more accurately.

Adds the idea that a prevalence can be for a taxid more specific than the overall one for a file.

Follow-up to https://github.com/naobservatory/p2ra/pull/53#discussion_r1181559803 and https://twist.com/a/197793/ch/661244/t/4356084/ 

Also fix a summary bug introduced in #73

pathogen_properties.py
@@ -42,6 +42,9 @@ class PathogenChars:
     na_type: NAType
     enveloped: Enveloped
     taxid: TaxID
+    # If we produce any estimates more specific than the overall taxid,
+    # subtaxids will contain all the secondary taxonomic ids we can generate.
+    subtaxids: frozenset[TaxID] = frozenset()
 
 
 def days_in_month(year: int, month: int) -> int:
@@ -69,6 +72,7 @@ class Variable:
     parsed_start: Optional[datetime.date] = field(init=False)
     parsed_end: Optional[datetime.date] = field(init=False)
     is_target: Optional[bool] = False
+    taxid: Optional[TaxID] = None
     inputs: InitVar[Optional[Iterable["Variable"]]] = None
     all_inputs: set["Variable"] = field(init=False)
 
@@ -152,6 +156,9 @@ class Variable:
         return ", ".join(bits)
 
     def summarize_location(self, all_locations=None):
+        if self.is_target and self._location():
+            return self._location()
+
         return "; ".join(
             sorted(
                 set(i._location() for i in self.all_inputs if i._location())
@@ -159,6 +166,9 @@ class Variable:
         )
 
     def summarize_date(self) -> Optional[tuple[datetime.date, datetime.date]]:
+        if self.is_target and self.parsed_start and self.parsed_end:
+            return self.parsed_start, self.parsed_end
+
         try:
             return min(
                 i.parsed_start for i in self.all_inputs if i.parsed_start

pathogens/norovirus.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import Counter, defaultdict
 
 import numpy as np
 
@@ -7,11 +7,15 @@ from pathogen_properties import *
 background = """Norovirus is a GI infection, mostly spread through personal
 contact."""
 
+NOROVIRUS = TaxID(142786)
+NOROVIRUS_GROUP_I = TaxID(122928)
+NOROVIRUS_GROUP_II = TaxID(122929)
 
 pathogen_chars = PathogenChars(
     na_type=NAType.RNA,
     enveloped=Enveloped.NON_ENVELOPED,
-    taxid=TaxID(142786),
+    taxid=TaxID(NOROVIRUS),
+    subtaxids=frozenset((NOROVIRUS_GROUP_I, NOROVIRUS_GROUP_II)),
 )
 
 # We're using Scallan 2011
@@ -68,7 +72,6 @@ shedding_duration = SheddingDuration(
     source="https://www.mayoclinic.org/diseases-conditions/norovirus/symptoms-causes/syc-20355296#:~:text=Norovirus%20infection%20symptoms%20usually%20last%201%20to%203%20days",
 )
 
-
 monthwise_count = dict[tuple[int, int], float]  # [year, month] -> float
 
 
@@ -81,8 +84,16 @@ def to_daily_counts(outbreaks_per_month: monthwise_count) -> monthwise_count:
     return outbreaks_per_day
 
 
-def load_nors_outbreaks() -> monthwise_count:
+def load_nors_outbreaks() -> (
+    tuple[monthwise_count, monthwise_count, monthwise_count]
+):
     us_outbreaks: monthwise_count = defaultdict(float)  # date -> count
+    us_outbreaks_I: monthwise_count = defaultdict(float)  # date -> count
+    us_outbreaks_II: monthwise_count = defaultdict(float)  # date -> count
+
+    # seen_I, seen_II -> count
+    totals: dict[tuple[bool, bool], int] = Counter()
+    total_seen_other = 0
 
     # Downloaded on 2023-04-28 from https://wwwn.cdc.gov/norsdashboard/
     # Click "Download all NORS Dashboard data (Excel)."
@@ -100,11 +111,11 @@ def load_nors_outbreaks() -> monthwise_count:
             year = int(row[cols.index("Year")])
             month = int(row[cols.index("Month")])
             state = row[cols.index("State")]
-            etiology = row[cols.index("Etiology")]
+            etiologies = row[cols.index("Etiology")]
             # It distinguishes between GI and GII Norovirus.  I'm currently
             # discarding this, but it could potentially be useful?
             genotype = row[cols.index("Serotype or Genotype")]
-            if "Norovirus" not in etiology:
+            if "Norovirus" not in etiologies:
                 # It's the National Outbreak Reporting System, not the
                 # Norovirus Outbreak Reporting System.
                 #
@@ -114,15 +125,53 @@ def load_nors_outbreaks() -> monthwise_count:
 
             date = year, month
 
-            us_outbreaks[date] += 1
+            seen_I = False
+            seen_II = False
+            seen_other = False
+            for etiology in etiologies.split("; "):
+                if etiology.endswith("Norovirus Genogroup I"):
+                    seen_I = True
+                elif etiology.endswith("Norovirus Genogroup II"):
+                    seen_II = True
+                elif "Genogroup" in etiology:
+                    seen_other = True
 
-    return us_outbreaks
+            us_outbreaks[date] += 1
+            if seen_I and not seen_II:
+                us_outbreaks_I[date] += 1
+            elif seen_II and not seen_I:
+                us_outbreaks_II[date] += 1
+
+            if seen_other:
+                total_seen_other += 1
+
+            # We don't care about the I-vs-II labeling in old data, so ignore
+            # dates before HISTORY_START.
+            if year >= HISTORY_START:
+                totals[seen_I, seen_II] += 1
+
+    total_classified = (
+        totals[True, False] + totals[False, True] + totals[True, True]
+    )
+
+    seen_both_fraction = totals[True, True] / total_classified
+
+    # As of 2023-05-03 this was 1.05%, low enough to ignore.  If this were
+    # higher we'd need to estimate prevalences that didn't add to the total
+    # prevalence.
+    assert seen_both_fraction < 0.011
+
+    # As of 2023-05-03 this was 0.15%, low enough to ignore.  If this were
+    # non-trivial we might want to try assigning reads to other genogroups.
+    assert total_seen_other / total_classified < 0.0015
+
+    return us_outbreaks, us_outbreaks_I, us_outbreaks_II
 
 
 def determine_average_daily_outbreaks(us_outbreaks: monthwise_count) -> float:
     total_us_outbreaks = 0.0
     days_considered = 0
-    for year in range(HISTORY_START, HISTORY_END + 1):
+    for year in range(HISTORY_START, COVID_START):
         for month in range(1, 13):
             total_us_outbreaks += us_outbreaks[year, month]
             days_considered += days_in_month(year, month)
@@ -134,13 +183,13 @@ def determine_average_daily_outbreaks(us_outbreaks: monthwise_count) -> float:
 #  * Long enough to reduce noise
 #  * Pre-covid
 HISTORY_START = 2012
-HISTORY_END = 2019
+COVID_START = 2020
 
 
 def estimate_prevalences():
     prevalences = []
 
-    us_outbreaks = load_nors_outbreaks()
+    us_outbreaks, us_outbreaks_I, us_outbreaks_II = load_nors_outbreaks()
     pre_covid_us_average_daily_outbreaks = determine_average_daily_outbreaks(
         us_outbreaks
     )
@@ -154,23 +203,58 @@ def estimate_prevalences():
 
     us_daily_outbreaks = to_daily_counts(us_outbreaks)
 
-    for year in range(HISTORY_START, HISTORY_END + 1):
+    for year in range(HISTORY_START, COVID_START):
         for month in range(1, 13):
             target_date = f"{year}-{month:02d}"
 
+            adjusted_national_prevalence = (
+                pre_covid_national_prevalence
+                * Scalar(
+                    scalar=us_daily_outbreaks[year, month]
+                    / pre_covid_us_average_daily_outbreaks,
+                    country="United States",
+                    date=target_date,
+                    source="https://wwwn.cdc.gov/norsdashboard/",
+                )
+            )
+
+            prevalences.append(
+                adjusted_national_prevalence.target(
+                    country="United States", date=target_date
+                )
+            )
+
+            # Assume that all Norovirus infections are either Group I or II,
+            # which is very close (see assertion above).  Also assume the
+            # outbreaks for which we have subtype info are representative of
+            # all infections.
+            us_I = us_outbreaks_I[year, month]
+            us_II = us_outbreaks_II[year, month]
+            if us_I + us_II:
+                group_I_fraction = us_I / (us_I + us_II)
+                group_II_fraction = us_II / (us_I + us_II)
+            else:
+                group_I_fraction = group_II_fraction = 0
+
             prevalences.append(
                 (
-                    pre_covid_national_prevalence
-                    * (
-                        Scalar(
-                            scalar=us_daily_outbreaks[year, month]
-                            / pre_covid_us_average_daily_outbreaks,
-                            country="United States",
-                            date=target_date,
-                            source="https://wwwn.cdc.gov/norsdashboard/",
-                        )
-                    )
-                ).target(country="United States", date=target_date)
+                    adjusted_national_prevalence
+                    * Scalar(scalar=group_I_fraction)
+                ).target(
+                    country="United States",
+                    date=target_date,
+                    taxid=NOROVIRUS_GROUP_I,
+                )
+            )
+            prevalences.append(
+                (
+                    adjusted_national_prevalence
+                    * Scalar(scalar=group_II_fraction)
+                ).target(
+                    country="United States",
+                    date=target_date,
+                    taxid=NOROVIRUS_GROUP_II,
+                )
             )
 
     return prevalences

summarize.py
@@ -48,7 +48,15 @@ def start(pathogen_names):
             location = estimate.summarize_location()
 
             prevalence = "%.2f per 100k" % estimate.infections_per_100k
-            line = "%s (%s; %s)" % (prevalence.rjust(18), location, date)
+            taxid = ""
+            if estimate.taxid:
+                taxid = "; %s" % estimate.taxid
+            line = "%s (%s; %s%s)" % (
+                prevalence.rjust(18),
+                location,
+                date,
+                taxid,
+            )
             to_print.append(((location, str(date)), line))
 
         to_print.sort()