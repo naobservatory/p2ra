9bc3758322ed69386c54cbdc34056a78c30c0ad3
Asher Parker-Sartori <aparkersartori@outlook.com>
2023-05-04 12:53:17 -0400

    Changed file based on Simon's suggestions

--- pathogens/west_nile_virus.py
+++ pathogens/west_nile_virus.py
@@ -6,9 +6,8 @@ background = """“West Nile virus (WNV) is the leading cause of mosquito-borne
 disease in the continental United States.  It is most commonly spread to 
 people by the bite of an infected mosquito. Cases of WNV occur during mosquito 
 season, which starts in the summer and continues through fall. There are no 
-vaccines to prevent or medications to treat WNV in people. Fortunately, most 
-people infected with WNV do not feel sick. About 1 in 5 people who are 
-infected develop a fever and other symptoms” (CDC)."""
+vaccines to prevent or medications to treat WNV in people. About 1 in 5 people 
+who are infected develop a fever and other symptoms” (CDC)."""
 
 
 pathogen_chars = PathogenChars(
@@ -20,6 +19,7 @@ pathogen_chars = PathogenChars(
 LA_county_cases_in_2020 = IncidenceAbsolute(
     annual_infections=90,
     tag="LA-2020",
+    date="2020",
     source="https://westnile.ca.gov/pdfs/VBDSAnnualReport20.pdf#?page=23",
 )
 
@@ -31,15 +31,18 @@ LA_county_population = Population(
 
 
 west_nile_duration = SheddingDuration(
+    # Symptoms last for 3-6 days usually, but sometimes for up to a month. I'm
+    # going to use 7 days as an estimate, since I cannot find a better source
+    # on this
     days=7,
     source="https://myhealth.alberta.ca/Health/aftercareinformation/pages/conditions.aspx?hwid=abo5809#:~:text=In%20mild%20cases%20of%20West%20Nile%2C%20symptoms%20usually%20last%20for%203%20to%206%20days%2C%20and%20you%20can%20recover%20at%20home.%20If%20you%20get%20a%20more%20severe%20case%20of%20West%20Nile%2C%20symptoms%20can%20last%20for%20weeks%20or%20months%2C%20and%20you%20may%20need%20to%20stay%20in%20the%20hospital%20so%20you%20can%20get%20medicine%20to%20help%20you%20recover.",
 )
 
-# 4/5 of people with WNV are asymptomatic, and therefore probably did not get
+# 3/4 of people with WNV are asymptomatic, and therefore probably did not get
 # tested
 asymptomatic_multiplier = Scalar(
-    scalar=5,
-    source="https://www.cdc.gov/westnile/symptoms/index.html",
+    scalar=4,
+    source="https://www.cdc.gov/mmwr/volumes/70/ss/ss7001a1.htm#:~:text=An%20estimated%2070%25%E2%80%9380%25%20of%20WNV%20infections%20are%20asymptomatic%20(8%2C9)",
 )
 
 
