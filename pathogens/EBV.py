29e350aaab379c3dadc3066fca952e27d87c6dde
Asher Parker-Sartori <aparkersartori@outlook.com>
2023-05-10 16:53:28 -0400

    Changed some stuff, still looking into adult esmt

--- pathogens/EBV.py
+++ pathogens/EBV.py
@@ -28,12 +28,13 @@ UK_seroprevalence_0_to_25 = Prevalence(
     country="UK",
     start_date="2002",
     end_date="2013",
+    number_of_participants=2325,
     active=Active.LATENT,
     source="https://bmcpublichealth.biomedcentral.com/articles/10.1186/s12889-020-09049-x#:~:text=1982/2325%20individuals%20(85.3%25)%20were%20EBV%20seropositive",
 )
 
 # Children ages 8-19
-nhanes_children_estimate = Prevalence(
+nhanes_age_8_19_estimate = Prevalence(
     infections_per_100k=0.665 * 100_000,
     confidence_interval=(0.643 * 100_000, 0.687 * 100_000),
     country="United States",
@@ -53,23 +54,23 @@ nhanes_18_19_yo_estimate = Prevalence(
     source="https://academic.oup.com/jid/article/208/8/1286/2192838#:~:text=years%2C%2069%25%3B%20and-,18%E2%80%9319%20years%2C%2089%25,-.%20Within%20each%20race",
 )
 
-US_child_proportion = Scalar(
+under_18_proportion_US = Scalar(
     scalar=0.222,
     date="2022",
     source="https://www.census.gov/quickfacts/fact/table/US#",
 )
 
-US_adult_proportion = Scalar(
-    scalar=1 - US_child_proportion.scalar,
+over_18_proportion_US = Scalar(
+    scalar=1 - under_18_proportion_US.scalar,
     date="2022",
     source="https://www.census.gov/quickfacts/fact/table/US#",
 )
 
 
 # this estimate uses children 18-19 as a proxy for the adult population
-us_seroprevalence_2003_2010 = nhanes_18_19_yo_estimate.__mul__(
-    US_adult_proportion
-).__add__(nhanes_children_estimate.__mul__(US_child_proportion))
+us_seroprevalence_2003_2010 = nhanes_18_19_yo_estimate * (
+    over_18_proportion_US
+) + nhanes_age_8_19_estimate * (under_18_proportion_US)
 
 
 def estimate_prevalences():
