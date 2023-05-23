0ce1f28be9626bc5b41180a5be443636815324e3
simonleandergrimm <simonleandergrimm@gmail.com>
2023-05-23 16:34:46 -0400

    Changed two things. First, I replaced the underreporting scalar with a CDC underestimation scalar that we also used for HCV. Second, I deleted king_county_absolute_* incidences. They aren't used, as the same source (https://doh.wa.gov/sites/default/files/2023-01/420-004-CDAnnualReport2021.pdf?uid=642c448518316#page=28) also provides rates per 100k for each county.

--- pathogens/hav.py
+++ pathogens/hav.py
@@ -36,33 +36,13 @@ us_population_2018 = Population(
 )
 
 incidence_underreporting_scalar = Scalar(
-    scalar=1 / 0.59,
-    confidence_interval=(
-        1 / 0.84,
-        1 / 0.32,
-    ),
+    scalar=2,
+    confidence_interval=(1.4,2.2),
     coverage_probability=0.95,
     country="United States",
-    source="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4906888/#:~:text=Diagnosed%20hepatitis%20A,clear%20reporting%20responsibilities.",
+    source="https://www.cdc.gov/hepatitis/statistics/2018surveillance/pdfs/2018HepSurveillanceRpt.pdf?#page=8",
 )
 
-king_county_absolute_2017 = IncidenceAbsolute(
-    annual_infections=11,
-    country="United States",
-    state="Washington",
-    county="King County",
-    date="2017",
-    source="https://doh.wa.gov/sites/default/files/2023-01/420-004-CDAnnualReport2021.pdf?uid=642c448518316#page=28",
-)
-
-king_county_absolute_2018 = IncidenceAbsolute(
-    annual_infections=14,
-    country="United States",
-    state="Washington",
-    county="King County",
-    date="2018",
-    source="https://doh.wa.gov/sites/default/files/2023-01/420-004-CDAnnualReport2021.pdf?uid=642c448518316#page=28",
-)
 
 king_county_confirmed_cases_rate_2017 = IncidenceRate(
     annual_infections_per_100k=0.5,
