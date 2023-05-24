9d84df3f7a331987a027f66be89edea15636bcf2
Asher Parker-Sartori <aparkersartori@outlook.com>
2023-05-24 13:33:38 -0400

    Improved comment on acute/chronic cases

--- pathogens/hbv.py
+++ pathogens/hbv.py
@@ -67,14 +67,13 @@ us_population_2019 = Population(
 
 
 ohio_total_incidence_2021 = IncidenceRate(
-    # This source reports both an acute and a total incidence. The total
-    # incidence is the sum of the acute and chronic incidences. I think it
-    # makes sense to report the total incidence, since chronic
+    # This source reports both an acute and a total (acute+chronic) incidence.
+    # I think it makes sense to report the total incidence, since chronic
     # cases still have an acute phase in which they shed. The number of
-    # chronic cases has some lag attached, since chronic cases are probably
-    # not identified during their acute phase. However, the reported chronic
-    # incidence in any given year should still be around the true number of
-    # new chronic cases for that year.
+    # chronic cases in any given year has some lag attached, since chronic
+    # cases are probably not identified during their acute phase. However, the
+    # reported chronic incidence in any given year should still be
+    # approximately the number of new chronic cases for that year.
     annual_infections_per_100k=16.5,
     country="United States",
     state="Ohio",
