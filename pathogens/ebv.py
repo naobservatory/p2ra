b8d75909b7efa980e51acc02d5149c26b5caebe7
simonleandergrimm <simonleandergrimm@gmail.com>
2023-06-16 12:16:34 -0400

    Adopted chagnes suggested by Jeff

--- pathogens/ebv.py
+++ pathogens/ebv.py
@@ -61,6 +61,8 @@ digit_extractor = re.compile("\d+")
 
 for race, cohort_dict in race_cohorts.items():
     with open(prevalence_data_filename(f"{race}.csv")) as inf:
+        # Data is downloaded from this results page: "https://data.census.gov/table?q=Annual+Estimates+of+the+Resident+Population+by+Single+Year".
+        # Age tables were renamed to [black/white/latino]_age_cohorts.csv.
         reader = csv.reader(inf)
 
         # Skip first two non-age lines
@@ -99,7 +101,7 @@ for race, cohort_dict in race_cohorts.items():
 EBV_US_2003_2010_SEROPREVALENCE = "https://academic.oup.com/jid/article/208/8/1286/2192838#:~:text=Table%201.Demographic%20Factors%20Associated%20With%20Epstein%E2%80%93Barr%20Virus%20(EBV)%20Antibody%20(Ab)%20Prevalence%2C%20by%20Race/Ethnicity%E2%80%94National%20Health%20and%20Nutrition%20Examination%20Survey%20Cycles%202003%E2%80%932004%2C%202005%E2%80%932006%2C%202007%E2%80%932008%2C%20and%202009%E2%80%932010"
 
 
-def us_seroprevalence_2003_2010():
+def us_seroprevalence_2020():
     with open(
         prevalence_data_filename("ebv_6_19_nhanes_2003_2010.csv")
     ) as inf:
@@ -169,20 +171,15 @@ def us_seroprevalence_2003_2010():
 
                 estimate_weights.append((prevalence, population))
 
-        us_seroprevalence_2003_2010 = Prevalence.weightedAverageByPopulation(
-            *estimate_weights
-        )
-
-    return us_seroprevalence_2003_2010
+    return Prevalence.weightedAverageByPopulation(*estimate_weights)
 
 
-# Source for CSV: https://www.census.gov/data-tools/demo/idb/#/pop?COUNTRY_YEAR=2023&COUNTRY_YR_ANIM=2023&FIPS_SINGLE=DA&menu=popViz&FIPS=DA&POP_YEARS=2023&popPages=BYAGE
-
-
-def denmark_seroprevalence_2023() -> Prevalence:
-    # We are applying the seroprevalence data from 1983 to current population
-    # data, given that there is no more recent seroprevalence data available.
+def load_denmark_populations_by_age() -> dict[tuple[int, int], Population]:
     with open(prevalence_data_filename("DenmarkPopulationData.csv")) as file:
+        # Source for CSV: https://www.census.gov/data-tools/demo/idb/#/pop?COUNTRY_YEAR=2023&COUNTRY_YR_ANIM=2023&FIPS_SINGLE=DA&menu=popViz&FIPS=DA&POP_YEARS=2023&popPages=BYAGE
+        # We are applying the seroprevalence data from 1983 to current
+        # population data, given that there is no more recent seroprevalence
+        # data available.
         denmark_age_groups: Counter[tuple[int, int]] = Counter()
         # (min_age, max_age) -> people within that age range
         reader = csv.reader(file)
@@ -193,28 +190,25 @@ def denmark_seroprevalence_2023() -> Prevalence:
             if row[4] == "TOTAL":
                 continue
 
-            age = int(
-                row[4].replace("+", "")
-            )  # Extract the age from the first column & turn "100+" into 100
-
-            # Remove spaces from the numbers and add them to the respective
-            # age group
-            population_by_age = int(row[5].replace(" ", ""))
-
+            # Extract the age from the first column & turn "100+" into 100
+            age = int(row[4].replace("+", ""))
             if age == 0:
-                denmark_age_groups[(0, 0)] += population_by_age
-            elif 1 <= age <= 3:
-                denmark_age_groups[(1, 3)] += population_by_age
-            elif 4 <= age <= 14:
-                denmark_age_groups[(4, 14)] += population_by_age
-            elif 15 <= age <= 17:
-                denmark_age_groups[(15, 17)] += population_by_age
-            elif 18 <= age <= 29:
-                denmark_age_groups[(18, 29)] += population_by_age
-            elif 30 <= age <= 100:
-                denmark_age_groups[(30, 100)] += population_by_age
+                age_group = 0, 0
+            elif age <= 3:
+                age_group = 1, 3
+            elif age <= 14:
+                age_group = 4, 14
+            elif age <= 17:
+                age_group = 15, 17
+            elif age <= 29:
+                age_group = 18, 29
+            elif age <= 100:
+                age_group = 30, 100
             else:
                 assert False
+            # Remove spaces from the numbers and add them to the respective
+            # age group
+            denmark_age_groups[age_group] += int(row[5].replace(" ", ""))
 
         denmark_populations = {}
         for (min_age, max_age), people in denmark_age_groups.items():
@@ -224,7 +218,10 @@ def denmark_seroprevalence_2023() -> Prevalence:
                 country="Denmark",
                 source="https://www.census.gov/data-tools/demo/idb/#/pop?COUNTRY_YEAR=2023&COUNTRY_YR_ANIM=2023&FIPS_SINGLE=DA&menu=popViz&FIPS=DA&POP_YEARS=2023&popPages=BYAGE",
             )
+    return denmark_populations
 
+
+def denmark_seroprevalence_2023() -> Prevalence:
     DENMARK_SEROPREVALENCE_SOURCE = "10.3109/inf.1983.15.issue-4.03"
 
     denmark_seroprevalences: dict[tuple[int, int], Prevalence] = {}
@@ -244,21 +241,24 @@ def denmark_seroprevalence_2023() -> Prevalence:
             source=DENMARK_SEROPREVALENCE_SOURCE,
         )
 
-    denmark_seroprevalence = Prevalence.weightedAverageByPopulation(
-        (denmark_seroprevalences[0, 0], denmark_populations[0, 0]),
-        (denmark_seroprevalences[1, 3], denmark_populations[1, 3]),
-        (denmark_seroprevalences[4, 14], denmark_populations[4, 14]),
-        (denmark_seroprevalences[15, 17], denmark_populations[15, 17]),
-        (denmark_seroprevalences[18, 29], denmark_populations[18, 29]),
-        (denmark_seroprevalences[30, 100], denmark_populations[30, 100]),
+    denmark_populations = load_denmark_populations_by_age()
+
+    seroprevalence_population_per_cohort = [
+        (denmark_seroprevalences[age], denmark_populations[age])
+        for age in denmark_populations.keys()
+    ]
+
+    return Prevalence.weightedAverageByPopulation(
+        *seroprevalence_population_per_cohort
     )
-    return denmark_seroprevalence
 
 
 def estimate_prevalences():
     denmark_2023 = denmark_seroprevalence_2023()
     # Seroprevalence should remain constant, so we can extrapolate from 1983
-    # data, applied to the 2023 Denmark population backwards to 2021-2019
+    # data, applied to the 2023 Denmark population backwards to 2021-2019.
+    # With this, we also assume that the infection rates per age cohort
+    # remained constant over time.
     denmark_2021 = dataclasses.replace(
         denmark_2023, date_source=Variable(date="2021")
     )
@@ -268,10 +268,9 @@ def estimate_prevalences():
     denmark_2019 = dataclasses.replace(
         denmark_2023, date_source=Variable(date="2019")
     )
-    # Seroprevalence should remain constant, so we can extrapolate from
-    # 2003-2010 data, applied to 2020 US population backwards to
-    # 2021.
-    us_2020 = us_seroprevalence_2003_2010()
+    # Similar to Denmark, seroprevalence should remain constant, so we can
+    # extrapolate from 2003-2010 data, applied to 2020 US population to 2021.
+    us_2020 = us_seroprevalence_2020()
     us_2021 = dataclasses.replace(us_2020, date_source=Variable(date="2021"))
 
     return [
