8fae3841e74eddc1275f8fec1b7f4144864c0b93
Jeff Kaufman <jeff@jefftk.com>
2023-07-14 15:53:56 -0400

    formatting

--- list_taxids.py
+++ list_taxids.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python3
-import pathogens
 import pathogen_properties
+import pathogens
+
 
 def start():
     taxid_to_name = {}
     for pathogen_name, pathogen in pathogens.pathogens.items():
         for taxids in pathogen_properties.by_taxids(
-                pathogen.pathogen_chars, pathogen.estimate_prevalences()):
+            pathogen.pathogen_chars, pathogen.estimate_prevalences()
+        ):
             for taxid in taxids:
                 taxid_to_name[taxid] = pathogen_name
         for taxids in pathogen_properties.by_taxids(
-                pathogen.pathogen_chars, pathogen.estimate_incidences()):
+            pathogen.pathogen_chars, pathogen.estimate_incidences()
+        ):
             for taxid in taxids:
                 taxid_to_name[taxid] = pathogen_name
 
     print("taxid", "filename", "human_readable", sep="\t")
     for taxid, name in sorted(taxid_to_name.items()):
         print(taxid, name, pathogens.tidy_name(name, [taxid]), sep="\t")
+
 
 if __name__ == "__main__":
     start()
