9c2c909aecc6d0757bfec96da2961e5b9eeda0f7
Jeff Kaufman <jeff@jefftk.com>
2023-07-14 10:43:02 -0400

    Add command list_taxids.py
    Example output:
    
    ```
    taxid           filename        human_readable
    10298           hsv_1           HSV-1
    10310           hsv_2           HSV-2
    10359           cmv             CMV
    10376           ebv             EBV
    10407           hbv             HBV
    10566           hpv             HPV
    10632           jcv             JCV
    10804           aav2            AAV2
    11103           hcv             HCV
    11320           influenza       Influenza A
    11520           influenza       Influenza B
    11676           hiv             HIV
    68558           aav6            AAV6
    82300           aav5            AAV5
    122928          norovirus       Norovirus (GI)
    122929          norovirus       Norovirus (GII)
    493803          mcv             MCV
    1891762         bkv             BKV
    2697049         sars_cov_2      SARS-COV-2
    ```
    
    I realized I needed this in trying to check that all of our taxids are
    recognizeable by Kraken, because there wasn't previously an easy way to get the
    taxids.

---
+++ list_taxids.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import pathogens
+import pathogen_properties
+
+def start():
+    taxid_to_name = {}
+    for pathogen_name, pathogen in pathogens.pathogens.items():
+        for taxids in pathogen_properties.by_taxids(
+                pathogen.pathogen_chars, pathogen.estimate_prevalences()):
+            for taxid in taxids:
+                taxid_to_name[taxid] = pathogen_name
+        for taxids in pathogen_properties.by_taxids(
+                pathogen.pathogen_chars, pathogen.estimate_incidences()):
+            for taxid in taxids:
+                taxid_to_name[taxid] = pathogen_name
+
+    print("taxid", "filename", "human_readable", sep="\t")
+    for taxid, name in sorted(taxid_to_name.items()):
+        print(taxid, name, pathogens.tidy_name(name, [taxid]), sep="\t")
+
+if __name__ == "__main__":
+    start()
