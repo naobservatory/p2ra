888ab7c1d38cdb7b54bf0b2c462224594c1aba19
Jeff Kaufman <jeff@securebio.org>
2023-08-08 11:37:48 -0400

    authors: check in script used to determine the set of authors (#228)
    * authors: check in script used to determine the set of authors across the papers we cite
    
    * formatting

---
+++ process-authors.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import re
+from collections import defaultdict
+
+by_last_name = defaultdict(set)
+all_authors = set()
+# Manually copied from papers
+with open("authors.txt") as inf:
+    for line in inf:
+        line = line.removesuffix("\n")
+        paper, authors = line.split(": ")
+
+        for author in authors.split(","):
+            author = author.strip()
+            author = author.removeprefix("and ")
+
+            for f, r in [
+                (".", ""),
+                ("Thomas Nordahl Petersen", "Thomas N Petersen"),
+                ("Christina Aaby Svendsen", "Christina A Svendsen"),
+            ]:
+                author = author.replace(f, r)
+
+            # remove middle names
+            while re.match(".* [A-Z] .*", author):
+                author = re.sub("(.+) [A-Z] (.+)", r"\1 \2", author)
+
+            by_last_name[author.split(" ")[-1]].add(author)
+            all_authors.add(author)
+
+for last_name in sorted(by_last_name):
+    if len(by_last_name[last_name]) == 1:
+        continue
+    print(last_name)
+    for author in sorted(by_last_name[last_name]):
+        print(" ", author)
+
+print(", ".join(sorted(all_authors)))
