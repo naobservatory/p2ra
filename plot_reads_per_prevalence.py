27b794299b29dff6020b12d76495b7b83a177a4d
simonleandergrimm <simonleandergrimm@gmail.com>
2023-11-02 11:32:12 -0400

    Added precentages to plot_read_per_prevalence.py.

--- plot_reads_per_prevalence.py
+++ plot_reads_per_prevalence.py
@@ -4,6 +4,7 @@ import csv
 from dataclasses import dataclass
 
 import matplotlib.pyplot as plt  # type: ignore
+
 import numpy as np
 
 PERCENTILES = [5, 25, 50, 75, 95]
@@ -49,43 +50,74 @@ def start():
         "crits_christoph": "Crits-Christoph",
         "spurbeck": "Spurbeck",
     }
-    fig, axes = plt.subplots(1, len(viruses), sharey=True, figsize=(9, 2.75))
-    for (virus, predictor_type), ax in zip(viruses, axes):
-        for i, study in enumerate(study_labels.keys()):
-            stats = data[virus, predictor_type, study, "Overall"]
-            median = stats.percentiles[50]
-            lower = stats.percentiles[25]
-            upper = stats.percentiles[75]
-            cumulative_incidence = np.logspace(-4, -1, 100)
-            detection_threshold = 100
+    fig, (top_axes, bottom_axes) = plt.subplots(
+        2, len(viruses), sharey=True, figsize=(9, 5.5)
+    )
 
-            color = f"C{i}"
+    for axes in top_axes, bottom_axes:
+        for (virus, predictor_type), ax in zip(viruses, axes):
+            for i, study in enumerate(study_labels.keys()):
+                stats = data[virus, predictor_type, study, "Overall"]
+                median = stats.percentiles[50]
+                lower = stats.percentiles[25]
+                upper = stats.percentiles[75]
+                cumulative_incidence = np.logspace(-4, -1, 100)
+                detection_threshold = 100 if axes is top_axes else 10
+                color = f"C{i}"
+                ax.set_box_aspect(0.9)
 
-            ax.loglog(
-                cumulative_incidence,
-                detection_threshold / (100 * median * cumulative_incidence),
-                color=color,
-                label=f"{study_labels[study]}",
-            )
-            ax.fill_between(
-                cumulative_incidence,
-                detection_threshold / (100 * lower * cumulative_incidence),
-                detection_threshold / (100 * upper * cumulative_incidence),
-                color=color,
-                alpha=0.2,
-            )
-            ax.set_title(virus)
-            ax.set_xlabel("Cumulative Incidence")
-            ax.grid()
-            ax.set_xticks([1e-4, 1e-3, 1e-2, 1e-1])
+                ax.loglog(
+                    cumulative_incidence,
+                    detection_threshold
+                    / (100 * median * cumulative_incidence),
+                    color=color,
+                    label=f"{study_labels[study]}",
+                )
 
-    fig.subplots_adjust(top=0.8)
-    for ax_number in 1, 2:
-        axes[ax_number].tick_params(
-            axis="y", which="both", left=False, right=False
-        )
+                ax.fill_between(
+                    cumulative_incidence,
+                    detection_threshold / (100 * lower * cumulative_incidence),
+                    detection_threshold / (100 * upper * cumulative_incidence),
+                    color=color,
+                    alpha=0.2,
+                )
+                if axes is top_axes:
+                    title_text = ax.text(
+                        0.5,
+                        1.05,
+                        virus,
+                        transform=ax.transAxes,
+                        horizontalalignment="center",
+                    )
 
-    axes[0].set_ylabel("Reads required for detection")
+                ax.grid()
+                ax.set_xticks([1e-4, 1e-3, 1e-2, 1e-1])
+                ax.set_xticklabels(["0.01%", "0.1%", "1%", "10%"])
+                ax.set_yticks([1e3, 1e6, 1e9, 1e12, 1e15])
+                ax.set_xlim(1e-4, 1e-1)
+
+    fig.subplots_adjust(hspace=0.4, wspace=0.2)
+
+    for i, (top_ax, bottom_ax) in enumerate(zip(top_axes, bottom_axes)):
+        if i == 0:
+            for ax in top_ax, bottom_ax:
+                ax.set_ylabel("Reads required for detection")
+        else:
+            for ax in top_ax, bottom_ax:
+                ax.tick_params(axis="y", which="both", left=False, right=False)
+
+        bottom_ax.set_xlabel("Cumulative Incidence")
+
+    top_ax_title = fig.axes[0].set_title(
+        "a",
+        fontweight="bold",
+    )
+    bottom_ax_title = fig.axes[3].set_title(
+        "b",
+        fontweight="bold",
+    )
+    for ax in top_ax_title, bottom_ax_title:
+        ax.set_position((-0.35, 1.05))
     legend = axes[1].legend(
         loc=(-0.5, -0.42),
         frameon=True,
@@ -94,6 +126,7 @@ def start():
         ncol=3,
     )
 
+    fig.tight_layout
     fig.savefig("reads_per_prevalence.png", bbox_inches="tight", dpi=600)
 
 
